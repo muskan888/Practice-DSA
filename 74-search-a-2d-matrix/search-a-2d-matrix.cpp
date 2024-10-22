1d16b06dea35b5d84c7246051b8100ebc96dabe9
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2024-10-22 18:08:26 -0400

    Time: 0 ms (100.00%) | Memory: 12.3 MB (39.16%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(int i=0;i<matrix.size();i++){
-            for(int j=0;j<matrix[0].size();j++){
-                if(matrix[i][j]==target){
-                    return true;
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+        for(int i=0;i<rows;i++){
+            if(matrix[i][cols-1]>=target){
+                //checking last element of each rw
+                //ans is in that row
+                //search in that row
+                //traverse columns
+                for(int j=0;j<cols;j++){
+                    if(matrix[i][j]==target){
+                        return true;
+                    }
                 }
             }
         }
