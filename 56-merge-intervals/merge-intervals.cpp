81aa60e3c83c329dbabfebd6bee19ba7cd29a28c
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2024-10-21 20:03:11 -0400

    Time: 4 ms (98.65%) | Memory: 23 MB (74.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -3,31 +3,16 @@ public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
         vector<vector<int>> ans;
         sort(intervals.begin(),intervals.end());
-    
-        int prev=intervals[0][0];
-        int curr=intervals[0][1];
-        
-        for(int i=1;i<intervals.size();i++){
-            if(curr>=intervals[i][0]){
-                if(curr<intervals[i][1]){
-                    curr=intervals[i][1];
-                }
+        for(int i=0;i<intervals.size();i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]){
+                ans.push_back(intervals[i]);
             }
-        
             else{
-                vector<int>temp;
-                temp.push_back(prev);
-                temp.push_back(curr);
-                ans.push_back(temp);
-                prev=intervals[i][0];
-                curr=intervals[i][1];
-              
+                ans.back()[1]=max(ans.back()[1],intervals[i][1]);
             }
         }
-        vector<int>temp;
-        temp.push_back(prev);
-        temp.push_back(curr);
-        ans.push_back(temp);
         return ans;
+
+    
     }
 };
