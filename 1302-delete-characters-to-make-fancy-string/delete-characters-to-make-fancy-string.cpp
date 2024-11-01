a47bad4e5f6c3e047012fa8e270400af25ffb6f6
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2024-11-01 15:14:43 -0400

    Time: 42 ms (15.58%) | Memory: 42.9 MB (10.70%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string makeFancyString(string s) {
+        string str="";
+        str.push_back(s[0]);
+        int ctr=1; //this ctr will track each alphabet
+
+        for(int i=1;i<s.size();i++){
+            if(s[i]==str.back()){
+                ctr++;
+                if(ctr<3){
+                    str.push_back(s[i]);
+                }
+            }
+            else{
+                ctr=1;
+                str.push_back(s[i]);
+            }
+        }
+
+        return str;
+    }
+};
