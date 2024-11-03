32481932282f32800d6b338c6c697aee841f13ec
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2024-11-03 00:19:01 -0400

    Time: 0 ms (100.00%) | Memory: 7.7 MB (37.13%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.size()!=goal.size()){
+            return false;
+        }
+        string doubled=s+s;
+        if(doubled.find(goal)<doubled.size()){
+            return true;
+        }
+        return false;
+    }
+};
