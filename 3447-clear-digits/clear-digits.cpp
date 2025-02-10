0402a8b2d60434160bb5fbd3facb0a7a81e7b8ee
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2025-02-10 10:20:34 -0500

    Time: 7 ms (1.97%) | Memory: 9.2 MB (27.61%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(auto i:s){
+            if(isdigit(i)){
+                st.pop();
+            }
+            else{
+                st.push(i);
+                cout<<i;
+            }
+        }
+        string ans="";
+        while(!st.empty()){
+            ans=st.top()+ans;
+            st.pop();
+        }
+        return ans;
+    }
+};
