988450f4eb5e54ab0658bc13ac4fec24a6d82e36
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2025-02-22 13:51:47 -0500

    Time: 4 ms (12.17%) | Memory: 17.2 MB (56.35%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -23,13 +23,29 @@ public:
         
 
     }
+    bool dfs(vector<vector<int>>&graph, int node, vector<int>&color, int currColor){
+        if(color[node]!=-1){
+            return color[node]==currColor;
+        }
+        color[node]=currColor;
+        for(int i=0;i<graph[node].size();i++){
+           if(! dfs(graph,graph[node][i],color,1-currColor)){
+            return false;
+           }
+        }
+        return true;
+
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int>color(graph.size(),-1);
         //graph can be disconnected so we check for every node
         for(int i=0;i<color.size();i++){
             if(color[i]==-1){
                 //this covers every starting node
-                if(!bfs(graph,i,color)){
+                // if(!bfs(graph,i,color)){
+                //     return false;
+                // }
+                if(!dfs(graph,i,color,0)){
                     return false;
                 }
             }
