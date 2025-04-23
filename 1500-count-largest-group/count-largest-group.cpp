95de2db5b39a2d8b401a949aaf0d792d563c0504
Muskan Dhingra <54903290+muskan888@users.noreply.github.com>
2025-04-22 22:01:28 -0400

    Time: 2 ms (49.15%) | Memory: 7.9 MB (74.92%) - LeetSync

---
+++ 1500-count-largest-group/count-largest-group.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int countLargestGroup(int n) {
+        map<int,int>mp;
+        for(int i=1;i<=n;i++){
+            {
+                int digit=i;
+                int sum=0;
+                while(digit>0){
+                    sum+=digit%10;
+                    digit=digit/10;
+                }
+                mp[sum]++;
+            }
+        }
+        int maxi=INT_MIN;
+        for(auto it:mp){
+            if(it.second>maxi){
+                maxi=it.second;
+            }
+        }
+       int ctr=0;
+        for(auto it:mp){
+            if(it.second==maxi){
+                ctr++;
+            }
+        }
+        return ctr;
+        
+    }
+};
